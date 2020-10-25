0d961475c58f215533f903fd707f36f60dbebbed
Baptiste Lamotte <lamotte.baptiste@gmail.com>
2020-10-25 15:03:34 +0100

    signup

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'front/build')));
 
 app.use('/', indexRouter);
 app.use('/talents', talentsRouter);
-app.use('restaurats', restaurantsRouter);
+app.use('/restaurants', restaurantsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
