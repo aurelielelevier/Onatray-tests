2335bc3d9ede0aef4c36abbfa9c2c906ab475ae8
Aurélie LE LEVIER <aurelie.lelevier@yahoo.fr>
2020-10-26 18:59:38 +0100

    listeresto

--- front/src/components/Talents/ListeRestaurants.js
+++ front/src/components/Talents/ListeRestaurants.js
@@ -1,16 +1,20 @@
 import React, {useState, useEffect} from 'react'
 import '../../App.less';
 import 'antd/dist/antd.less';
-import { Layout, Card, Row, Button, Checkbox} from 'antd';
+import { Layout, Card, Row, Button, Checkbox, Col, Select, Form } from 'antd';
 import { Map, TileLayer, Marker, Popup, Polygon } from 'react-leaflet';
 import HeaderScreen from '../Header';
 import HeaderTalent from '../HeaderTalent'
 import L from 'leaflet';
 
 const { Content } = Layout;
+const { Option } = Select
+
+
+
 
 function ListeRestaurants(){
-    const [polygone, setPolygone] = useState([])
+    
     // il faudra faire une requette en BDD au chargement pour savoir si déjà défini par le Talent et que le 
     // périmètre soir dessiné au chargement et puisse être modifié
     const [polygoneinverse, setPolygoneinverse] = useState([])
@@ -18,7 +22,16 @@ function ListeRestaurants(){
     const[adresse, setAdresse] = useState('')
     const[adressesProposees, setAdressesProposees] = useState('')
     const [latlngDomicile, setLatlngDomicile] = useState([48.8534, 2.3488])
+    const [typePrix, setTypePrix] = useState([])
+    const [typeCuisine, setTypeCuisine] = useState([])
+    const [typeAmbiance, setAmbiance] = useState([])
 
+    const Submitform = values => {
+        console.log('Received values of form:', values);
+        console.log(typeAmbiance)
+        console.log(typeCuisine)
+        };
+    
     useEffect(() => {
        // fecth get pour obtenir le latlng de l'adresse et le polygone défini par l'utilisateur
        // gérer l'absence de périmètre défini
@@ -45,28 +58,53 @@ function ListeRestaurants(){
         
         <HeaderTalent/>
 
-        <Row style={{ marginTop: '50px', textAlign:'center', justifyContent:'center'}}>
-           
-        </Row>
+            <Row style={{backgroundColor:"#4B6584", height:"150px", display:"flex", justifyContent:"center", alignItems:'center', marginBottom:"15px"}}>
+                
+                <Col span={18} >
+                    <Form.Item label="Type de clientele : ">
+                        <Select 
+                            onChange={(e)=>setAmbiance(e)}
+                            style={{width:'80%'}}
+                            mode='multiple'
+                            name={'clientele'}
+                            className="basic-multi-select"
+                            classNamePrefix="select">
+                        <Option value='touristique'>Touristique</Option>
+                        <Option value='quartier'>De Quartier</Option>
+                        <Option value='jeune'>Jeune</Option>
+                        <Option value='agée'>Agée</Option>
+                    
+                        </Select>
+                    </Form.Item>
 
-        <Row style={{justifyContent:'center'}}>
-           
-        </Row>
+                    <Form.Item label="Type de clientele : ">
+                        <Select 
+                            onChange={(e)=>setTypeCuisine(e)}
+                            style={{width:'80%'}}
+                            mode='multiple'
+                            name={'clientele'}
+                            className="basic-multi-select"
+                            classNamePrefix="select">
+                        <Option value='touristique'>Touristique</Option>
+                        <Option value='quartier'>De Quartier</Option>
+                        <Option value='jeune'>Jeune</Option>
+                        <Option value='agée'>Agée</Option>
+                    
+                        </Select>
+                    </Form.Item>
 
-        <Row style={{justifyContent:'center', marginTop:'30px'}}>
-            
-            <p>(cliquez plusieurs fois pour dessiner les points de contours de votre choix)</p>
-        
-        </Row>
+                <Button onClick={Submitform()} type="primary" > Rechercher</Button>
+                </Col>
+            </Row>
 
-        
+
         <Row>
            
             <Content style={{ padding: '0 24px', minHeight: 280}}>
 
                 <Card style={{ width: '100%', textAlign:'center', backgroundColor:'#fed330', marginTop:'30px' }}>
                 <div>
-                    <Map center={latlngDomicile} zoom={12} onClick={(e) => {setPolygone([...polygone, [e.latlng.lat, e.latlng.lng]]); setPolygoneinverse([...polygoneinverse, [e.latlng.lng, e.latlng.lat]]); console.log(adresse)}}>
+                    <Map center={latlngDomicile} zoom={12} onClick={(e) => { console.log(e)}}>
                         <TileLayer
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -75,7 +113,7 @@ function ListeRestaurants(){
                             {/* {markers.map((user,i)=>{ 
                             return (<Marker position={user}></Marker>)
                             })} */}
-                        <Polygon positions={polygone} color="red" />
+
                         <Marker position={latlngDomicile}>
                             <Popup> Mon domicile <br/></Popup>
                         </Marker>
