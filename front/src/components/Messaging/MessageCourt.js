dda29dd54041c775bd7bbe4b96c064ca2572f3c0
Aurélie LE LEVIER <aurelie.lelevier@yahoo.fr>
2020-10-26 18:04:07 +0100

    listemssages

--- front/src/components/Messaging/MessageCourt.js
+++ front/src/components/Messaging/MessageCourt.js
@@ -3,33 +3,36 @@ import 'antd/dist/antd.less';
 import '../../App.less';
 import {Link} from 'react-router-dom';
 import {Avatar,  Comment, Tooltip} from 'antd';
+import moment from 'moment';
 
 function MessageCourt(props){
+    
     const [color, setColor] = useState('#ffffff')
 
     return(
 
     <Link to={'/messageRoom'}>
+        
         <Comment
-            onMouseEnter={()=> setColor("#a5b1c2")}
+            onMouseEnter={()=> setColor("#d1d8e0")}
             onMouseLeave={()=> setColor("#ffffff")} 
             style={{border:'1px solid #4b6584', textAlign:'left', backgroundColor:color, borderRadius:10, boxShadow:'5px 5px 5px #4b6584'}}
             // actions={[<span key="comment-basic-reply-to">Répondre</span>]}
-            author={<a>{props.message.nom}</a>}
+            author={<a>{props.nom}</a>}
             avatar={
                 <Avatar
-                src={props.message.avatar}
-                alt={props.message.nom}
+                src={props.avatar}
+                alt={props.nom}
                 />
             }
             content={
                 <p>
-                {props.message.contenu}
+                {props.contenu}
                 </p>
             }
             datetime={
-                <Tooltip title={props.moment().format('YYYY-MM-DD HH:mm:ss')}>
-                <span>{props.message.date}</span>
+                <Tooltip title={moment().format('YYYY-MM-DD HH:mm:ss')}>
+                <span>{props.date}</span>
                 {/* <span>{moment().fromNow()}</span> */}
                 </Tooltip>
             }
@@ -37,7 +40,4 @@ function MessageCourt(props){
         </Link>
     )}
 
-    export default connect(
-        null, 
-        mapDispatchToProps
-      )(MessageCourt);
+    export default MessageCourt;
