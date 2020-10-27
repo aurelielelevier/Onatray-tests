c80748587c9c4caf10edd53b317a01c4530e4512
Aurélie LE LEVIER <aurelie.lelevier@yahoo.fr>
2020-10-27 17:25:45 +0100

    modif liste resto pour tri

--- routes/talents.js
+++ routes/talents.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var restaurantModel = require('../model/restaurants')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -10,11 +11,85 @@ router.post('/createAccount', function(req,res,next){
 console.log('its body',req.body)
 })
 
-router.post('/envoi-secteur', function(req, res, next){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+router.post('/envoi-secteur', async function(req, res, next){
   console.log('requete reçue par le back')
-  // var listePoints = await JSON.parse(req.body.liste)
-  // listePoints.push(listePoints[0]) 
-  // console.log(listePoints)
+  var listePoints = await JSON.parse(req.body.liste)
+  listePoints.push(listePoints[0]) 
+  console.log(listePoints)
+
 
   // var result = await userModel.find(
   //   {
@@ -33,6 +108,42 @@ router.post('/envoi-secteur', function(req, res, next){
   //   }
   // )
   res.json([[48.866667, 2.333333], [48.830745, 2.2135133588867095], [48.807913541763654, 2.4624223554687408]])
+})
+
+router.get(`/cherche-liste-restaurant`, async function(req, res, next){
+  console.log('requête reçue')
+  var response = await restaurantModel.find()
+  console.log(response)
+  res.json(response)
+})
+
+router.post(`/recherche-liste-restaurants`, async function(req, res, next){
+  var restaurant = JSON.parse(req.body.restaurant)
+  console.log(restaurant)
+  var responseAenvoyer = await restaurantModel.find(
+    {
+          // adresseLngLat: {
+          //    $geoIntersects: {
+          //       $geometry: {
+          //          type: "Polygon" ,
+          //          coordinates: [ zone ],
+          //          crs: {
+          //             type: "name",
+          //             properties: { name: "urn:x-mongodb:crs:strictwinding:EPSG:4326" }
+          //          }
+          //       }
+          //    }
+          // },
+          typeOfFood : { $in: restaurant.cuisine},
+          //cuisine : { $in: restaurant.cuisine} ,
+          //typeOfRestaurant : { $in: restaurant.type} ,
+          //clientele: { $in: restaurant.ambiance} ,
+          // prix :{ $in: [restaurant.prix]} 
+        }
+  )
+  
+  console.log(responseAenvoyer, 'réponse !!!!!')
+  res.json(responseAenvoyer)
 })
 
 module.exports = router;
