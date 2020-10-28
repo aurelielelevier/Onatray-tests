b68c72de70507ceed77219e7e2c93e04dcbaedf4
Aurélie LE LEVIER <aurelie.lelevier@yahoo.fr>
2020-10-28 10:39:25 +0100

    gestion conflits

--- front/src/components/Messaging/MessageCourt.js
+++ front/src/components/Messaging/MessageCourt.js
@@ -8,31 +8,10 @@ import moment from 'moment';
 function MessageCourt(props){
     
     const [color, setColor] = useState('#ffffff')
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> signin
-// import React, { useState, useEffect } from 'react';
-// import 'antd/dist/antd.less';
-// import '../../App.less';
-// import {Link} from 'react-router-dom';
-// import {Avatar,  Comment, Tooltip} from 'antd';
 
-// function MessageCourt(props){
-//     const [color, setColor] = useState('#ffffff')
-
-<<<<<<< HEAD
    return(
 
-=======
 
- return(
-
->>>>>>> d4bf7a03c7d6839cdd4cf3fa6912989d688566b0
-=======
-    return(
-
->>>>>>> signin
     <Link to={'/messageRoom'}>
         
         <Comment
@@ -62,43 +41,7 @@ function MessageCourt(props){
         </Link>
     )}
 
-<<<<<<< HEAD
-    export default MessageCourt;
-//     <Link to={'/messageRoom'}>
-//         <Comment
-//             onMouseEnter={()=> setColor("#a5b1c2")}
-//             onMouseLeave={()=> setColor("#ffffff")} 
-//             style={{border:'1px solid #4b6584', textAlign:'left', backgroundColor:color, borderRadius:10, boxShadow:'5px 5px 5px #4b6584'}}
-//             // actions={[<span key="comment-basic-reply-to">Répondre</span>]}
-//             author={<a>{props.message.nom}</a>}
-//             avatar={
-//                 <Avatar
-//                 src={props.message.avatar}
-//                 alt={props.message.nom}
-//                 />
-//             }
-//             content={
-//                 <p>
-//                 {props.message.contenu}
-//                 </p>
-//             }
-//             datetime={
-//                 <Tooltip title={props.moment().format('YYYY-MM-DD HH:mm:ss')}>
-//                 <span>{props.message.date}</span>
-//                 {/* <span>{moment().fromNow()}</span> */}
-//                 </Tooltip>
-//             }
-//         />
-//         </Link>
-//     )}
 
-//     export default connect(
-//         null, 
-//         mapDispatchToProps
-//       )(MessageCourt);
-<<<<<<< HEAD
-=======
-    export default MessageCourt
->>>>>>> d4bf7a03c7d6839cdd4cf3fa6912989d688566b0
-=======
->>>>>>> signin
+export default MessageCourt
+
+
